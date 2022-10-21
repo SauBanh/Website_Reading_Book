87c4001c732f6a04537d4461b3d7108b49de01e0
Nguyễn Tuấn Anh <ngtuananh.developer@gmail.com>
2022-10-21 22:36:21 +0700

    update css

--- assets/js/PageHome/notiffy.js
+++ assets/js/PageHome/notiffy.js
@@ -1,10 +1,10 @@
 // dùng để xóa dấu chấm khi có thông báo đến
     document.getElementsByClassName('center js_show_hide')[0].onclick = function(){
-    document.getElementsByClassName('js_seen')[0].classList.remove('seen');
-    document.getElementsByClassName('box_info_user')[0].classList.remove('active_box_info_user');
-    document.getElementsByClassName('js_fun_show_hide')[0].classList.toggle('js_show_notify');
-    document.getElementsByClassName('notification_box')[0].onclick = function(e){
-        e.stopPropagation();
+        document.getElementsByClassName('js_seen')[0].classList.remove('seen');
+        document.getElementsByClassName('box_info_user')[0].classList.remove('active_box_info_user');
+        document.getElementsByClassName('js_fun_show_hide')[0].classList.toggle('js_show_notify');
+        document.getElementsByClassName('notification_box')[0].onclick = function(e){
+            e.stopPropagation();
     }
     // xóa cái active thông báo đi 
     var li = document.getElementsByClassName('item_notify_header');
@@ -16,6 +16,7 @@
             }
         }
     }
+    // đánh dấu là đã đọc tất cả các thông báo
     document.getElementsByClassName('list_notify_header')[0].scrollTop = - document.getElementsByClassName('list_notify_header')[0].scrollHeight;
     document.getElementsByClassName('notification_header')[0].classList.toggle('notification_header_active_js');
     document.getElementsByClassName('see_all_notify')[0].onclick = function(){
@@ -29,13 +30,36 @@
     }
 }
 
-function test(){
-    document.getElementsByClassName('box_info_user')[0].classList.remove('active_box_info_user');
-    console.log('hello')
-}
-
+// function activeOverlay(){
+//     document.getElementsByClassName('overlay')[0].classList.add('block_overlay');
+//     console.log('hello')
+// }
+// activeOverlay();
+// xử lý đóng mở user 
 document.getElementsByClassName('user_header')[0].onclick = function(){
     document.getElementsByClassName('box_info_user')[0].classList.toggle('active_box_info_user');
     document.getElementsByClassName('notification_header')[0].classList.remove('notification_header_active_js');
     document.getElementsByClassName('js_fun_show_hide')[0].classList.remove('js_show_notify');
+    // document.getElementsByClassName('overlay')[0].classList.add('block_overlay');
+    // activeOverlay();
 }
+
+// xử lý khi đóng các box lên khi click ra ngoài box 
+const modal = document.getElementsByClassName('notification_box')[0];
+document.getElementsByClassName('main')[0].onclick = function(e){
+    let clickInside = modal.contains(e.target)
+    if(clickInside){
+        modal.classList.remove('js_show_notify');
+        console.log(modal)
+    }
+}
+
+// const modal = document.querySelector('.modal')           box
+
+// document.addEventListener('click', (e) => {              main
+//   let clickInside = modal.contains(e.target)             box
+
+//   if (!clickInside) {                                    box
+//      modal.classList.remove('show')                      box
+//   }
+// })
