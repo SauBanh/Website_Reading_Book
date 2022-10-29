d243187865d1e4b4a972622fb2f6a4c684306591
Nguyễn Tuấn Anh <ngtuananh.developer@gmail.com>
2022-10-29 16:04:27 +0700

    done follow btn of item book

--- B2CNode/src/public/js/PageHome/follow.js
+++ B2CNode/src/public/js/PageHome/follow.js
@@ -1,13 +1,16 @@
 var btnFollow = document.querySelectorAll('.item_book_icon_follow');
 var btnFollow_Length = btnFollow.length;
-console.log(btnFollow[0])
 
 {/* <i class="fa-solid fa-bookmark"></i> fa-regular fa-bookmark */}
 for(var i = 0; i < btnFollow_Length; i++){        
     btnFollow[i].onclick = function(){
-        if(this.classList.contains('fa-solid')){
-            this.classList.replace('fa-solid', 'fa-regular');            
+        if(this.classList.contains('fa-regular')){
+            this.classList.remove('fa-regular')
+            this.classList.add('fa-solid');
         }
-        console.log(this)
+        else{
+            this.classList.remove('fa-solid');
+            this.classList.add('fa-regular')
+        }
     }
 }
