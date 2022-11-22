95fbd554f1c2dc5a1e79d2ce4b040ad933598187
Cuongvlvlvlvl <doquoccuong81@yahoo.com.vn>
2022-11-22 13:41:12 +0700

    add view

--- B2CNode/src/app/controllers/BooksController.js
+++ B2CNode/src/app/controllers/BooksController.js
@@ -1,11 +1,28 @@
 const Book = require('../models/Books');
 const Chap = require('../models/Chapters');
+const View = require('../models/Views');
 
 class BooksController {
   //xong trả về đọc truyện
   async index(req, res) { 
 
     var thisbook = await Book.findOne({slug: req.params.slug});
+
+    //check if user viewed this book
+    if(req.isAuthenticated()){
+      const isView = await View.findOne({bookid: thisbook._id.toString(), viewer: req.user._id.toString()});
+      if(!isView){
+        var viewer = new View();
+        viewer.bookid = thisbook._id.toString();
+        viewer.viewer = req.user._id.toString();
+        viewer.save();
+        await sleep(50);
+      }
+    }
+
+    //count how many people has viewed this book
+    const viewNumber = await View.find({bookid: thisbook._id.toString()}).count();    
+
     //check xem phai truyen vip ko
     if(thisbook.vip == true){
       //check xem nguoi dung da dang nhap chua
@@ -20,13 +37,13 @@ class BooksController {
         } else {
           var chaps = await Chap.find({bookid: thisbook._id});
           chaps = chaps.map(chap => chap.toObject());
-          res.render('book', {session: req.user, thisbook: thisbook.toObject(), lstchap: chaps});
+          res.render('book', {session: req.user, thisbook: thisbook.toObject(), lstchap: chaps, viewNumber});
         }
       }
     } else {
       var chaps = await Chap.find({bookid: thisbook._id});
       chaps = chaps.map(chap => chap.toObject());
-      res.render('book', {session: req.user, thisbook: thisbook.toObject(), lstchap: chaps});
+      res.render('book', {session: req.user, thisbook: thisbook.toObject(), lstchap: chaps, viewNumber});
     }
 
   }
@@ -62,6 +79,12 @@ class BooksController {
     res.json(lstBooks);
   }
 
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 module.exports = new BooksController();
