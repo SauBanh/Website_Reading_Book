a5af28ac76668805a47b3d5b3f715ab33f09c597
Cuongvlvlvlvl <doquoccuong81@yahoo.com.vn>
2022-11-23 23:58:12 +0700

    fix bug

--- B2CNode/src/routes/read.js
+++ B2CNode/src/routes/read.js
@@ -7,6 +7,14 @@ router.get('/', ctl.apiBooks);
 
 router.get('/:slug', ctl.index);
 
+router.get('/:slug/delete', ctl.bookDelete);
+
 router.get('/:slug/:chap', ctl.chap);
+
+router.get('/:slug/:chap/delete', ctl.chapDelete);
+
+router.get('/:slug/:chap/previous', ctl.previousChap);
+
+router.get('/:slug/:chap/next', ctl.nextChap);
 
 module.exports = router;
