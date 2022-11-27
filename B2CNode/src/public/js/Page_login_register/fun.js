6a5984858cc9579276c575dc02e5751d28d1dd01
Nguyễn Tuấn Anh <69725247+SauBanh@users.noreply.github.com>
2022-11-27 17:11:22 +0700

    update fix page resgister

--- B2CNode/src/public/js/Page_login_register/fun.js
+++ B2CNode/src/public/js/Page_login_register/fun.js
@@ -1,78 +1,5 @@
 
-const toggle = document.getElementsByClassName("toggle"),
 
-      input = document.getElementsByClassName("imput-conect");
-  toggle[0].addEventListener("click", () =>{
-  if(input[0].type === "password") {
-    input[0].type = "text";
-    toggle[0].classList.replace("fa-eye-slash", "fa-eye");
-  }else {
-    input[0].type = "password";
-    toggle[0].classList.replace("fa-eye", "fa-eye-slash");
-  }
-  })
-  toggle[1].addEventListener("click", () =>{
-    if(input[1].type === "password") {
-      input[1].type = "text";
-      toggle[1].classList.replace("fa-eye-slash", "fa-eye");
-    }else {
-      input[1].type = "password";
-      toggle[1].classList.replace("fa-eye", "fa-eye-slash");
-    }
-    })
-    toggle[2].addEventListener("click", () =>{
-      if(input[2].type === "password") {
-        input[2].type = "text";
-        toggle[2].classList.replace("fa-eye-slash", "fa-eye");
-      }else {
-        input[2].type = "password";
-        toggle[2].classList.replace("fa-eye", "fa-eye-slash");
-      }
-      })
-
-
-var username= document.forms['form']['username'];
-var password= document.forms['form']['password'];
-var email= document.forms['form']['email'];
-var confirmpassword= document.forms['form']['confirm'];
-var currentpassword= document.forms['form']['currentpassword'];
-
-var error_user= document.getElementById('error_user');
-var error_password= document.getElementById('error_password');
-var error_email= document.getElementById('error_email');
-var error_confirmpassword= document.getElementById('error_confirmpassword');
-var error_currentpassword= document.getElementById('error_currentpassword');
-
-username.addEventListener('textInput',user_veryfy);
-password.addEventListener('textInput',pass_veryfy);
-email.addEventListener('textInput',email_veryfy);
-confirmpassword.addEventListener('textInput',confirm_veryfy);
-currentpassword.addEventListener('textInput',current_veryfy);
-
-function validator_register(){
-
-    if(email.value.length <9){
-      error_email.style.display="block";
-      email.focus();
-      return false;      
-    }
-    if(username.value.length <9){
-      error_user.style.display="block";
-      username.focus();
-      return false;      
-    }
-    if(password.value.length <6){
-      error_password.style.display="block";
-      password.focus();
-      return false; 
-    }
-    
-    if(confirmpassword.value.length <6){
-      error_confirmpassword.style.display="block";
-      confirmpassword.focus();
-      return false; 
-    }
-}
 function validator_login(){
 
   if(username.value.length <9){
@@ -145,5 +72,3 @@ function confirm_veryfy(){
     return true;
   }
 }
-
-
